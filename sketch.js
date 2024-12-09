d548c962560531b26f15b294d4f0cbd6f92241a0
Michael Ramón <michael.ramon@ucuenca.edu.ec>
2024-12-09 14:38:08 -0500

    Ajuste
    Se hizo pantalla completa y se cambio el fondo

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(220, 10, 50);
 }
 
 function draw() {
   circle(mouseX, mouseY, random(0, 100));
-  // background(220);
 }
